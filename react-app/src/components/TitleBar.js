b1c6bc9c4664055d96fb415f341cc2d16835c764
ivanktanujaya <ivanktanujaya@gmail.com>
2021-09-14 15:18:02 +0200

    Added TitleBar

---
+++ react-app/src/components/TitleBar.js
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import React from 'react';
+
+export default function TitleBar(){
+    return(
+        <Container>
+            <h1>Welcome to the Prolog Mutator</h1>
+        </Container>
+    )
+}
