93bfe377c7108670a79103b74a8b6ab58aae1491
ivanktanujaya <ivanktanujaya@gmail.com>
2021-09-17 16:36:32 +0200

    ResultSection now able to pass data to the Table Component

--- react-app/src/components/ResultSection.js
+++ react-app/src/components/ResultSection.js
@@ -10,6 +10,15 @@ class ResultSection extends React.Component{
         this.state={
             showSpinner:true,
             showTable:false,
+            items: [
+                {id:1, name:'File1', type:'A', result:'0'},
+                {id:2, name:'File2', type:'A', result:'1'},
+                {id:3, name:'File3', type:'B', result:'0'},
+                {id:4, name:'File4', type:'C', result:'0'},
+                {id:5, name:'File5', type:'B', result:'1'},
+                {id:6, name:'File6', type:'A', result:'1'},
+                {id:7, name:'File7', type:'B', result:'0'},
+            ],
         }
         this.handleMutButtonClick = this.handleMutButtonClick.bind(this)
     }
@@ -26,7 +35,7 @@ class ResultSection extends React.Component{
             <div>
                 <Button as="input" type="submit" value="Start Mutation" className="my-3" onClick={this.handleMutButtonClick} />{''}
                 {this.state.showSpinner && <LoadingSpinner/>}
-                {this.state.showTable && <ResultTable/>}
+                {this.state.showTable && <ResultTable items={this.state.items} />}
             </div>
             
         )
