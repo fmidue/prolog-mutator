a86f9fc195c37eeff94544df8aff797b8920945f
ivanktanujaya <ivanktanujaya@gmail.com>
2021-10-28 13:15:40 +0200

    Added a new key pair to identify external Mutations

--- react-app/src/MutationRegistry.js
+++ react-app/src/MutationRegistry.js
@@ -2,11 +2,13 @@ const disjConjMut = require('./assets/DisjConjMut')
 const relOpMut = require('./assets/RelOpMut')
 const ariOpMut = require('./assets/AriOpMut')
 const predNegMut = require('./assets/PredNegMut')
+const externalMutation = require('./assets/ExternalMutation')
 
 const mutationRegistry = {
     ConjDisjMut:{
         name : "Conjunction to Disjunction",
         enable: true,
+        external: false,
         mutation: function (obj,mode){
             return disjConjMut.disjConjMut(obj,mode,"ConjDisj")
         },
@@ -14,6 +16,7 @@ const mutationRegistry = {
     DisjConjMut:{
         name  : "Disjunction to Conjunction",
         enable: true,
+        external: false,
         mutation: function (obj,mode){
             return disjConjMut.disjConjMut(obj,mode,"DisjConj")
         },
@@ -21,6 +24,7 @@ const mutationRegistry = {
     RelOpMut:{
         name : "Relational Operator Mutation",
         enable: true,
+        external: false,
         mutation: function (obj,mode){
             return relOpMut.relOpMut(obj,mode)
         },
@@ -28,6 +32,7 @@ const mutationRegistry = {
     AriOpMut:{
         name : "Arithmetical Operator Mutation",
         enable: true,
+        external: false,
         mutation: function (obj,mode){
             return ariOpMut.ariOpMut(obj,mode)
         },
@@ -35,9 +40,18 @@ const mutationRegistry = {
     PredNegMut:{
         name : "Predicate Negation Mutation",
         enable: true,
+        external: false,
         mutation: function (obj,mode){
             return predNegMut.predNegMut(obj,mode)
         },
+    },
+    DropClauseMutation:{
+        name : "Drop Clause Mutation",
+        enable: true,
+        external: true,
+        mutation: async function(file,mode){
+            return await externalMutation.externalMutation(file,mode,"drop-clause-mutation")
+        }
     },
 }
 
