d6b0a4bbd48a4e205ac7bf07e0242eeac2d24b0c
ivanktanujaya <ivanktanujaya@gmail.com>
2021-11-03 22:49:04 +0100

    Implemented Reverse Negation for Summary Mode

--- react-app/src/assets/PredNegMut.js
+++ react-app/src/assets/PredNegMut.js
@@ -7,7 +7,7 @@ function predNegMut(textAndOpObj,mode){
         let mutantArr = performIndividualMutations(textAndOpObj.realText,textAndOpObj.predIndex, textAndOpObj.predicates, mode.num)
         result = result.concat(mutantArr)
     }else if (mode.mode==="summ"){
-        let mutantArr = performSummarilyMutations(textAndOpObj.realText,textAndOpObj.predIndex,mode.num)
+        let mutantArr = performSummarilyMutations(textAndOpObj.realText,textAndOpObj.predIndex,textAndOpObj.predicates,mode.num)
         result = result.concat(mutantArr)
     }
     return result;
@@ -20,7 +20,7 @@ function performIndividualMutations(text,indexArr,predicates,numMutant){
         if(predicates[i].includes("\\+")){
             negationExist = true
         }
-        let mutantText = negateSinglePredicate(text,[indexPair],negationExist)
+        let mutantText = negateSinglePredicate(text,[indexPair],[negationExist])
         mutantArray.push(mutantText);
     })
     
@@ -32,7 +32,7 @@ function performIndividualMutations(text,indexArr,predicates,numMutant){
     return mutantArray;
 }
 
-function performSummarilyMutations(text,indexArr,numMutant){
+function performSummarilyMutations(text,indexArr,predicates,numMutant){
     let mutantArray = []
     let numPred = indexArr.length
     //Check for Mutant Amount. Max 1000
@@ -41,24 +41,28 @@ function performSummarilyMutations(text,indexArr,numMutant){
             let variationArr = replM.replicateM(numPred,[0,1],true) 
             variationArr.forEach((x)=>{
                 var mutIndexArr = []
+                let negArray = []
                 for (var i = 0; i < numPred; i++){
                     if (x[i]===1){
                         mutIndexArr.push(indexArr[i])
+                        predicates[i].includes("\\+") ? negArray.push(true) : negArray.push(false)
                     }
                 }
-                var mutantText = negateSinglePredicate(text,mutIndexArr)
+                var mutantText = negateSinglePredicate(text,mutIndexArr,negArray)
                 mutantArray.push(mutantText)
             })
         }else{
             while (mutantArray.length < numMutant){
                 let variationArr = helper.generateRandomChar(numPred,[0,1])
                 var mutIndexArr = []
+                let negArray = []
                 for (var i =0; i < variationArr.length; i++){
                     if(variationArr[i]===1){
                         mutIndexArr.push(indexArr[i])
+                        predicates[i].includes("\\+") ? negArray.push(true) : negArray.push(false)
                     }
                 }
-                let mutantText = negateSinglePredicate(text,mutIndexArr) 
+                let mutantText = negateSinglePredicate(text,mutIndexArr,negArray) 
                 if(!mutantArray.includes(mutantText)){
                     mutantArray.push(mutantText)
                 }
@@ -72,20 +76,31 @@ function negateSinglePredicate(text,indexArr,negExist){
     if (indexArr[0][0] > text.length-1 ||indexArr.length === 0 ){
         return text;
     }else{
-        if (indexArr.length === 1 && !negExist){
+        if (indexArr.length === 1 && !negExist[0]){
             return text.substring(0,indexArr[0][0]) + " \\+ " + text.substring(indexArr[0][0],text.length)
-        } else if (indexArr.length === 1 && negExist){
+        } else if (indexArr.length === 1 && negExist[0]){
             return text.substring(0,indexArr[0][0]) + text.substring(indexArr[0][0]+2,text.length)
         }
         let returnText = ""
         for(var i = 0; i < indexArr.length ; i++){
             if (i === 0){
-                returnText += text.substring(0, indexArr[i][0]) + " \\+ "
+                if(negExist[i]){
+                    returnText += text.substring(0,indexArr[i][0]) + text.substring(indexArr[i][0]+2,indexArr[i+1][0])
+                }else{
+                    returnText += text.substring(0, indexArr[i][0]) + " \\+ " + text.substring(indexArr[i][0],indexArr[i+1][0])
+                }
             }else if(i === indexArr.length-1){
-                returnText += text.substring(indexArr[i-1][0],indexArr[i][0]) + " \\+ "
-                returnText += text.substring(indexArr[i][0],text.length)
+                if(negExist[i]){
+                    returnText += text.substring(indexArr[i][0]+2,text.length)
+                }else{
+                    returnText += " \\+ " + text.substring(indexArr[i][0],text.length) 
+                }
             }else{
-                returnText += text.substring(indexArr[i-1][0],indexArr[i][0]) + " \\+ "
+                if(negExist[i]){
+                    returnText += text.substring(indexArr[i][0]+2,indexArr[i+1][0])
+                }else{
+                    returnText += " \\+ " + text.substring(indexArr[i][0],indexArr[i+1][0]) 
+                }
             }
         }
         return returnText;
