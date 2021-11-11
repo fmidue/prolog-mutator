d3066901fcae9e160f1f795dc30303a4a58909a9
ivanktanujaya <ivanktanujaya@gmail.com>
2021-11-11 20:23:25 +0100

    Replaced option with num in externalMutation

--- react-app/src/assets/ExternalMutation.js
+++ react-app/src/assets/ExternalMutation.js
@@ -2,9 +2,9 @@
 var FormData = require('form-data');
 const fetch = require('node-fetch');
 
-async function externalMutation(textFile,opt,endPoint){
+async function externalMutation(textFile,num,endPoint){
     var resArr
-    var route = opt.num.toString()
+    var route = num.toString()
     const data = new FormData()
     data.append('program', textFile)
     await fetch(`http://localhost:8080/${endPoint}/${route}`,{
